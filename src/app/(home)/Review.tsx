33b4701243128fb0380052208085bb6aad156a86
Uzoanya Dominic <nomsouzoanya@yahoo.co.uk>
2024-02-15 17:10:09 +0100

    Update Review to be Responsive

--- src/app/(home)/Review.tsx
+++ src/app/(home)/Review.tsx
@@ -8,7 +8,9 @@ import {
     ImageListItem,
     Rating,
     Stack,
-    Typography
+    Typography,
+    useMediaQuery,
+    useTheme
 } from '@mui/material';
 import { makeStyles } from 'tss-react/mui';
 
@@ -22,11 +24,13 @@ const useStyles = makeStyles()(theme => ({
         alignItems: 'center',
 
         [theme.breakpoints.down('md')]: {
+            gridTemplateColumns: '1fr',
             marginTop: 0,
             padding: theme.spacing(6.9, 5),
         },
 
         [theme.breakpoints.down('sm')]: {
+            gap: theme.spacing(5),
             padding: theme.spacing(6.9, 3)
         }
     },
@@ -51,119 +55,121 @@ const useStyles = makeStyles()(theme => ({
     },
 
     image: {
-        width: '100%',
-        height: 'auto'
+        objectFit: 'cover',
+        width: '100%'
     }
 }));
 
 const Review: React.FC<{}> = () => {
     const { classes } = useStyles();
+    const theme = useTheme();
+    const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
 
     return (
         <Box component="section" className={classes.root}>
-                <Stack direction="column" alignItems="center" spacing={4} sx={{ textAlign: 'center' }}>
-                    <Typography variant="h5" className={classes.title}>What they say about us</Typography>
-                    <Avatar src="/img/user-review.jpg" sx={{ width: 90, height: 90 }} />
-                    <Rating readOnly value={4} />
-                    <Typography variant="h6" className={classes.subTitle}>Slate helps you see how many more days you need to work to reach your financial goal.</Typography>
-                    <Box component="div">
-                        <Typography variant="body2" className={classes.text} color="primary">Regina Miles</Typography>
-                        <Typography variant="body2" className={classes.text}>Designer</Typography>
-                    </Box>
-                </Stack>
-                <ImageList cols={3} rowHeight={142} gap={17.85}>
-                    <ImageListItem>
-                        <Image 
-                            src="/img/review1.png"
-                            width={142}
-                            height={142}
-                            alt="Review"
-                            priority
-                            className={classes.image}
-                        />
-                    </ImageListItem>
-                    <ImageListItem>
-                        <Image 
-                            src="/img/review2.png"
-                            width={142}
-                            height={142}
-                            alt="Review"
-                            priority
-                            className={classes.image}
-                        />
-                    </ImageListItem>
-                    <ImageListItem>
-                        <Image 
-                            src="/img/review3.png"
-                            width={142}
-                            height={142}
-                            alt="Review"
-                            priority
-                            className={classes.image}
-                        />
-                    </ImageListItem>
-                    <ImageListItem>
-                        <Image 
-                            src="/img/review4.png"
-                            width={142}
-                            height={142}
-                            alt="Review"
-                            priority
-                            className={classes.image}
-                        />
-                    </ImageListItem>
-                    <ImageListItem>
-                        <Image 
-                            src="/img/review5.png"
-                            width={142}
-                            height={142}
-                            alt="Review"
-                            priority
-                            className={classes.image}
-                        />
-                    </ImageListItem>
-                    <ImageListItem>
-                        <Image 
-                            src="/img/review6.png"
-                            width={142}
-                            height={142}
-                            alt="Review"
-                            priority
-                            className={classes.image}
-                        />
-                    </ImageListItem>
-                    <ImageListItem>
-                        <Image 
-                            src="/img/review7.png"
-                            width={142}
-                            height={142}
-                            alt="Review"
-                            priority
-                            className={classes.image}
-                        />
-                    </ImageListItem>
-                    <ImageListItem>
-                        <Image 
-                            src="/img/review8.png"
-                            width={142}
-                            height={142}
-                            alt="Review"
-                            priority
-                            className={classes.image}
-                        />
-                    </ImageListItem>
-                    <ImageListItem>
-                        <Image 
-                            src="/img/review9.png"
-                            width={142}
-                            height={142}
-                            alt="Review"
-                            priority
-                            className={classes.image}
-                        />
-                    </ImageListItem>
-                </ImageList>
-            </Box>
+            <Stack direction="column" alignItems="center" spacing={4} sx={{ textAlign: 'center' }}>
+                <Typography variant="h5" className={classes.title}>What they say about us</Typography>
+                <Avatar src="/img/user-review.jpg" sx={{ width: 90, height: 90 }} />
+                <Rating readOnly value={4} />
+                <Typography variant="h6" className={classes.subTitle}>Slate helps you see how many more days you need to work to reach your financial goal.</Typography>
+                <Box component="div">
+                    <Typography variant="body2" className={classes.text} color="primary">Regina Miles</Typography>
+                    <Typography variant="body2" className={classes.text}>Designer</Typography>
+                </Box>
+            </Stack>
+            <ImageList cols={3} rowHeight={isMobile ? 100: 142} gap={17.85}>
+                <ImageListItem>
+                    <Image 
+                        src="/img/review1.png"
+                        width={isMobile ? 100 : 142}
+                        height={isMobile ? 100: 142}
+                        alt="Review"
+                        priority
+                        className={classes.image}
+                    />
+                </ImageListItem>
+                <ImageListItem>
+                    <Image 
+                        src="/img/review2.png"
+                        width={isMobile ? 100 : 142}
+                        height={isMobile ? 100: 142}
+                        alt="Review"
+                        priority
+                        className={classes.image}
+                    />
+                </ImageListItem>
+                <ImageListItem>
+                    <Image 
+                        src="/img/review3.png"
+                        width={isMobile ? 100 : 142}
+                        height={isMobile ? 100: 142}
+                        alt="Review"
+                        priority
+                        className={classes.image}
+                    />
+                </ImageListItem>
+                <ImageListItem>
+                    <Image 
+                        src="/img/review4.png"
+                        width={isMobile ? 100 : 142}
+                        height={isMobile ? 100: 142}
+                        alt="Review"
+                        priority
+                        className={classes.image}
+                    />
+                </ImageListItem>
+                <ImageListItem>
+                    <Image 
+                        src="/img/review5.png"
+                        width={isMobile ? 100 : 142}
+                        height={isMobile ? 100: 142}
+                        alt="Review"
+                        priority
+                        className={classes.image}
+                    />
+                </ImageListItem>
+                <ImageListItem>
+                    <Image 
+                        src="/img/review6.png"
+                        width={isMobile ? 100 : 142}
+                        height={isMobile ? 100: 142}
+                        alt="Review"
+                        priority
+                        className={classes.image}
+                    />
+                </ImageListItem>
+                <ImageListItem>
+                    <Image 
+                        src="/img/review7.png"
+                        width={isMobile ? 100 : 142}
+                        height={isMobile ? 100: 142}
+                        alt="Review"
+                        priority
+                        className={classes.image}
+                    />
+                </ImageListItem>
+                <ImageListItem>
+                    <Image 
+                        src="/img/review8.png"
+                        width={isMobile ? 100 : 142}
+                        height={isMobile ? 100: 142}
+                        alt="Review"
+                        priority
+                        className={classes.image}
+                    />
+                </ImageListItem>
+                <ImageListItem>
+                    <Image 
+                        src="/img/review9.png"
+                        width={isMobile ? 100 : 142}
+                        height={isMobile ? 100: 142}
+                        alt="Review"
+                        priority
+                        className={classes.image}
+                    />
+                </ImageListItem>
+            </ImageList>
+        </Box>
     );
 };
 
